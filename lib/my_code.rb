c17822ea24b660a22eb862926e2248e7888ce190
Jabair Khan <jabairk@gmail.com>
2021-02-04 22:26:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,7 +12,7 @@ end
 def reduce(source_array, starting_point = 0)
   new_value = ""
   source_array.each do |source_array.sum|
-    new_value << source_array
+    new_value << yield 
   while index < source_array.length
   new_value.push(yield(source_array[index, starting_point]))
                 # ^General yield here, as this lab needs a generalized method to solve all 6 questions at once.  
